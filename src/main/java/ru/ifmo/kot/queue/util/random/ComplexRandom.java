2b7db798ddae465461fbf0953eaa7ce4d6dbe10e
Ilia Kisliakovskii <kisliakovskiii@yandex.ru>
2016-04-03 14:52:25 +0300

    Generator tests

---
+++ src/main/java/ru/ifmo/kot/queue/util/random/ComplexRandom.java
@@ -0,0 +1,249 @@
+package ru.ifmo.kot.queue.util.random;
+
+
+import static java.lang.Math.log;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+
+/**
+ * Note: The author has to use double type for all generators, because
+ * the correctness of the generators is determined by using MatLab
+ */
+@SuppressWarnings("WeakerAccess")
+public class ComplexRandom {
+
+    /**
+     * start values
+     */
+    public static final int SEED_1 = 676475;
+    public static final int SEED_2 = 565645;
+
+    /**
+     * the number of values the sequence is formed of
+     */
+    public static final int m = 2_147_483_647;
+    /**
+     * "multiplier"
+     */
+    public static final int a = 630_360_016;
+    /**
+     * the number of sequence values
+     */
+    public static final int n = 1000;
+
+    /**
+     * the quantile of a standard normal distribution of order of "1-α / 2" // todo update
+     * α=1-β - a significance level (α = 0.05)
+     */
+    public static final double u = 1.96;
+
+    private final double[] sequence;
+    private final double mean;
+    private final double variance;
+    private final double[] covariance;
+    private final double[] correlation;
+    private final double[] interval = new double[2];
+
+    public enum Type {
+        STANDARD, EXPONENTIAL
+    }
+
+    public ComplexRandom() {
+        this(SEED_1);
+    }
+
+    public ComplexRandom(Type type) {
+        this(SEED_1, type);
+    }
+
+    public ComplexRandom(final int seed) {
+        this(seed, Type.STANDARD);
+    }
+
+    public ComplexRandom(final int seed, Type type) {
+        switch (type) {
+            case EXPONENTIAL:
+                sequence = exponentialRandomSequence(seed);
+                break;
+            default:
+                sequence = standardUniformlyDistributedRandomSequence(seed);
+        }
+        mean = mean(sequence);
+        variance = variance(sequence);
+        covariance = covariance(sequence);
+        correlation = correlation(sequence);
+        final double accuracy = accuracy(sequence);
+        interval[0] = mean - accuracy;
+        interval[1] = mean + accuracy;
+    }
+
+    public double[] sequence() {
+        return sequence;
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double variance() {
+        return variance;
+    }
+
+    public double[] covariance() {
+        return covariance;
+    }
+
+    public double[] correlation() {
+        return correlation;
+    }
+
+    public double[] interval() {
+        return interval;
+    }
+
+    public static double nextValue(final double number) {
+        return nextValue(number, a, m);
+    }
+
+    private static double nextValue(final double number, final int a, final int m) {
+        return (a * number) % m;
+    }
+
+    public static double normalizeValue(final double value) {
+        return normalizeValue(value, m);
+    }
+
+    private static double normalizeValue(final double value, final int m) {
+        return value / m;
+    }
+
+    private double exponizedValue(final double value) {
+        return  -mean * log(value);
+    }
+
+    public static double[] uniformlyDistributedRandomSequence(
+            final int seed, final int a,
+            final int m, final int n) {
+        double[] sequence = new double[n];
+        sequence[0] = seed;
+        for (int i = 1; i < n; i++) {
+            sequence[i] = nextValue(sequence[i - 1], a, m);
+        }
+        return sequence;
+    }
+
+    public static double[] uniformlyDistributedRandomSequence(final int seed) {
+        return uniformlyDistributedRandomSequence(seed, a, m, n);
+    }
+
+    public static double[] uniformlyDistributedRandomSequence() {
+        return uniformlyDistributedRandomSequence(SEED_1, a, m, n);
+    }
+
+    public static double[] standardUniformlyDistributedRandomSequence(
+            final int seed, final int a,
+            final int m, final int n) {
+        double[] prevSequence = uniformlyDistributedRandomSequence(seed, a, m, n);
+        double[] sequence = new double[n];
+        for (int i = 0; i < n; i++) {
+            sequence[i] = normalizeValue(prevSequence[i]);
+        }
+        return sequence;
+    }
+
+    public static double[] standardUniformlyDistributedRandomSequence(final int seed) {
+        return standardUniformlyDistributedRandomSequence(seed, a, m, n);
+    }
+
+    public static double[] standardUniformlyDistributedRandomSequence() {
+        return standardUniformlyDistributedRandomSequence(SEED_1, a, m, n);
+    }
+
+    public double[] exponentialRandomSequence(final int seed) {
+        double[] prevSequence = standardUniformlyDistributedRandomSequence(seed);
+        double[] sequence = new double[n];
+        for (int i = 0; i < n; i++) {
+            sequence[i] = exponizedValue(prevSequence[i]);
+        }
+        return sequence;
+    }
+
+    public static double mean(double[] sequence) {
+        double sum = 0;
+        for (double num: sequence) {
+            sum += num;
+        }
+        return sum/sequence.length;
+    }
+
+    public static double variance(double[] sequence) {
+        double mean = mean(sequence);
+        double sum = 0;
+        for (double num: sequence) {
+            sum += pow(num - mean, 2);
+        }
+        return sum/(sequence.length - 1);
+    }
+
+    public static double standardDeviation(double[] sequence) {
+        return sqrt(variance(sequence));
+    }
+
+    public static double[] covariance(double[] prevSequence) {
+        final int n = prevSequence.length;
+        final double mean = mean(prevSequence);
+        double[] sequence = new double[n - 1];
+        for (int j = 1; j < sequence.length + 1; j++) {
+            double sum = 0;
+            for (int i = 0; i < n - j; i++) {
+                sum += (prevSequence[i] - mean) * (prevSequence[i + j] - mean);
+            }
+            sequence[j - 1] = sum / (n - j);
+        }
+        return sequence;
+    }
+
+    public static double[] correlation(double[] prevSequence) {
+        double variance = variance(prevSequence);
+        double[] covariance = covariance(prevSequence);
+        double[] sequence = new double[covariance.length];
+        for (int i = 0; i < sequence.length; i++) {
+            sequence[i] = covariance[i] / variance;
+        }
+        return sequence;
+    }
+
+    public static double meanSquaredDisplacement (double[] sequence) {
+        return sqrt(variance(sequence) / sequence.length);
+    }
+
+    private static double accuracy(double[] sequence) {
+        return meanSquaredDisplacement(sequence) * u;
+    }
+
+    public static double[] interval(double[] sequence) {
+        final double mean = mean(sequence);
+        final double accuracy = accuracy(sequence);
+        return new double[]{mean - accuracy, mean + accuracy};
+    }
+
+    public String sequenceToString(double[] sequence) {
+        StringBuilder builder = new StringBuilder();
+        for (double num: sequence) {
+            builder.append(num);
+            builder.append(" ");
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Sequence: " + sequenceToString(sequence) + "\n" +
+                "Mean: " + mean + "\n" +
+                "Variance: " + variance + "\n" +
+                "Covariance: " + sequenceToString(covariance) + "\n" +
+                "Correlation: " + sequenceToString(correlation) + "\n" +
+                "Interval: " + "[" + interval[0] + ";" + interval[1] + "]" + "\n";
+    }
+}
---
+++ src/test/java/ru/ifmo/kot/queue/util/random/ComplexRandomTest.java
@@ -0,0 +1,168 @@
+package ru.ifmo.kot.queue.util.random;
+
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+import org.apache.commons.math3.stat.descriptive.moment.Variance;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static ru.ifmo.kot.queue.util.random.ComplexRandom.*;
+
+public class ComplexRandomTest {
+
+    private static final double DOUBLE_DELTA = 0.00001;
+    private static final int commonSeed = SEED_1;
+    private static final double[] commonSequence =
+            standardUniformlyDistributedRandomSequence(commonSeed);
+
+    @Test
+    public void checkGenerationOfValue() {
+        // values were obtained empirically
+        final double currentValue = 676475.0;
+        final double expectedNextValue = 1.259006104e+9;
+
+        final double actualNextValue = nextValue(currentValue);
+
+        assertEquals("The next value was generated wrong.",
+                expectedNextValue, actualNextValue, DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkNormalizationOfValue() {
+        // values were obtained empirically
+        final double currentValue = 676475.0;
+        final double expectedNormalizedValue = 3.150082194781901e-4;
+
+        final double actualNormalizedValue = normalizeValue(currentValue);
+
+        assertEquals("The value was normalized wrong.",
+                expectedNormalizedValue, actualNormalizedValue, DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkUniformlyDistributedRandomSequence() {
+
+        final double[] uniDistRandSeq =
+                uniformlyDistributedRandomSequence(SEED_1, a, m, n);
+
+        assertEquals("Even the first value does not equal the seed",
+                uniDistRandSeq[0], SEED_1, DOUBLE_DELTA);
+
+        final int indexOfCheck = 65;
+        assertEquals("The value " + "#" + indexOfCheck + " does not equal the checked value",
+                uniDistRandSeq[indexOfCheck],
+                iterativeNextValue(indexOfCheck, SEED_1),
+                DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkStandardUniformlyDistributedRandomSequence() {
+        final double[] uniDistRandSeq =
+                uniformlyDistributedRandomSequence(SEED_1, a, m, n);
+        final double[] stdUniDistRandSeq =
+                standardUniformlyDistributedRandomSequence(SEED_1, a, m, n);
+
+        int indexOfCheck = 999;
+        assertEquals("The value " + "#" + indexOfCheck + "was normalized wrong",
+                stdUniDistRandSeq[indexOfCheck],
+                normalizeValue(uniDistRandSeq[indexOfCheck]),
+                DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkMean() {
+        final double expectedMean = new Mean().evaluate(commonSequence);
+        final double actualMean = mean(commonSequence);
+        assertEquals("The mean was calculated wrong",
+                expectedMean,
+                actualMean,
+                DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkVariance() {
+        final double expectedVariance = new Variance().evaluate(commonSequence);
+        final double actualVariance = variance(commonSequence);
+        assertEquals("The variance was calculated wrong",
+                expectedVariance,
+                actualVariance,
+                DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkStandardDeviation() {
+        final double expectedSd = new StandardDeviation().evaluate(commonSequence);
+        final double actualSd = standardDeviation(commonSequence);
+        assertEquals("The standard deviation was calculated wrong",
+                expectedSd,
+                actualSd,
+                DOUBLE_DELTA);
+    }
+
+
+    @Test
+    public void checkCovariance() {
+        // values were obtained empirically
+        final int seed = 676475;
+        final int indexOfCheck = 998;
+        final double expectedValue = -0.154098281730902; // for the index that specified above
+
+        final double[] stdUniDistRandSeq =
+                standardUniformlyDistributedRandomSequence(seed);
+
+        assertEquals("The value " + "#" + indexOfCheck + " does not equal the checked value",
+                expectedValue,
+                covariance(stdUniDistRandSeq)[indexOfCheck],
+                DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkCorrelation() {
+        // values were obtained empirically
+        final int seed = 676475;
+        final int firstIndexOfCheck = 0;
+        final double firstExpectedValue = 0.016064724191234; // for the index that specified above
+        final int secondIndexOfCheck = 998;
+        final double secondExpectedValue = -1.738026692492030; // for the index that specified above
+
+        final double[] stdUniDistRandSeq =
+                standardUniformlyDistributedRandomSequence(seed);
+        final double[] actualCorrelation = correlation(stdUniDistRandSeq);
+
+        assertEquals("The value " + "#" + firstIndexOfCheck + " does not equal the checked value",
+                firstExpectedValue,
+                actualCorrelation[firstIndexOfCheck],
+                DOUBLE_DELTA);
+
+        assertEquals("The value " + "#" + secondIndexOfCheck + " does not equal the checked value",
+                secondExpectedValue,
+                actualCorrelation[secondIndexOfCheck],
+                DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkInterval() {
+        final double[] actualInterval = interval(commonSequence);
+        System.out.println("Interval: [" + actualInterval[0] + ", " + actualInterval[1] + "]");
+    }
+
+    @Test
+    public void checkNonStaticStandardUniformlyDistributedRandomSequence() {
+        final ComplexRandom random = new ComplexRandom(SEED_1, Type.STANDARD);
+        assertEquals("The mean was calculated wrong",
+                new Mean().evaluate(random.sequence()), random.mean(), DOUBLE_DELTA);
+        assertEquals("The variance was calculated wrong",
+                new Variance().evaluate(random.sequence()), random.variance(), DOUBLE_DELTA);
+    }
+
+//////////////////////auxiliary methods////////////////////////
+
+    private double iterativeNextValue(int index, double seed) {
+        double value = seed;
+        for (int i = 0; i < index; i++) {
+            value = nextValue(value);
+        }
+        return value;
+    }
+
+}
