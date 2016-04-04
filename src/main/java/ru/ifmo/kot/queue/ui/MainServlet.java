03fb3b98c863ff9956288d3b23048080f737e4ab
Ilia Kisliakovskii <kisliakovskiii@yandex.ru>
2016-04-04 07:58:56 +0300

    servlet

--- src/main/java/ru/ifmo/kot/queue/ui/MainServlet.java
+++ src/main/java/ru/ifmo/kot/queue/ui/MainServlet.java
@@ -2,20 +2,26 @@ package ru.ifmo.kot.queue.ui;
 
 import org.eclipse.jetty.http.MimeTypes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        Map<String, String> map = new HashMap<>();
+        map.put("property", "undefined");
+        String json = new ObjectMapper().writeValueAsString(map);
         response.setContentType(MimeTypes.Type.APPLICATION_JSON.toString());
-        response.getWriter().write(
-                "{" + "property: 'undefined'" + "}");
+        response.setContentType(MimeTypes.Type.APPLICATION_JSON.toString());
+        response.getWriter().write(json);
     }
-
 }
